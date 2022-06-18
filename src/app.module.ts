de00f11069f888e4f876686e3c2acd5e18a3a1d3
Hyxotis <chris.chich@hotmail.fr>
2022-06-18 19:09:26 +0200

    :sparkles: Added sample controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MyControllerController } from './my-controller/my-controller.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, MyControllerController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/my-controller/my-controller.controller.ts
@@ -0,0 +1,92 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  Redirect,
+} from '@nestjs/common';
+import { IsArray, IsNotEmpty, Max, Min } from 'class-validator';
+
+class MyDto {
+  @IsNotEmpty()
+  name: string;
+
+  @Min(0)
+  @Max(120)
+  age: number;
+
+  @IsArray()
+  grades: number[];
+}
+
+@Controller('my-controller')
+export class MyControllerController {
+  @Get()
+  get(): string {
+    return 'GET on root';
+  }
+
+  @Get('with-query')
+  getQueryParam(@Query('param') param: string): string {
+    return `Sent: ${param}`;
+  }
+
+  @Post()
+  post(@Body('param') param: string): string {
+    return `Sent ${param}`;
+  }
+
+  @Get('test-*')
+  getWithWildcard(): string {
+    return 'This route uses a wildcard';
+  }
+
+  @Get('code')
+  @HttpCode(201)
+  getWithHttpCode(): string {
+    return 'Return code is 201';
+  }
+
+  @Get('header')
+  @Header('Cache-Control', 'none')
+  getWithSpecificHeader(): string {
+    return 'Cache-Control set to none';
+  }
+
+  @Get('redirect')
+  @Redirect('/')
+  getWithRedirect(@Query('route') route: string) {
+    if (route !== '') {
+      return {
+        url: route,
+      };
+    }
+  }
+
+  @Get('with-param/:param')
+  getURLParam(@Param('param') param: string): string {
+    return `Param set to ${param}`;
+  }
+
+  @Get('async')
+  getAsynchronously(@Query('fail') fail: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (fail === 'true') {
+          return reject();
+        }
+
+        return resolve('Returned in 3s');
+      }, 3000);
+    });
+  }
+
+  @Post('dto')
+  postUsingDto(@Body() myDto: MyDto): MyDto {
+    return myDto;
+  }
+}
